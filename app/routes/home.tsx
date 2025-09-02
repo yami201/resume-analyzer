e2204098c3e54a05ab171d9624c1c34c4f9506ee
yami201 <mohamednj2013@gmail.com>
2025-09-02 14:27:39 +0100

    assets and initial css

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeIQ" },
+    { name: "description", content: "Smart feedback for your resume!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Welcome to ResumeIQ</div>;
 }
