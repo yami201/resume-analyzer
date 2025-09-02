b6dc0c15a6a9905de3c13acf95370101742ab47a
yami201 <mohamednj2013@gmail.com>
2025-09-02 15:19:29 +0100

    add home page and resume cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
+import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/resume-card";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "ResumeIQ" },
     { name: "description", content: "Smart feedback for your resume!" },
@@ -8,5 +11,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <div>Welcome to ResumeIQ</div>;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-10">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your resume and get instant AI-powered feedback!</h2>
+        </div>
+
+      {
+        resumes.length > 0 && (
+          <div className="resumes-section">
+            {
+              resumes.map((resume: Resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))
+            }
+          </div>
+        )
+      }
+      </section>
+
+    </main>
+  );
 }
